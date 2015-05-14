39dc3e0f1937cc2ce513d569ea0855b73c6f7c55
Vishal Vijay <vishalvijay@jiffstore.com>
2015-05-14 16:04:23 +0530

    option to force production url

--- lib/payuindia.rb
+++ lib/payuindia.rb
@@ -9,8 +9,12 @@ module PayuIndia
   self.test_url = 'https://test.payu.in/_payment.php'
   self.production_url = 'https://secure.payu.in/_payment.php'
 
-  def self.service_url
-    defined?(Rails) && Rails.env == 'production' ? self.production_url : self.test_url
+  def self.service_url force_production = false
+    unless force_production
+      defined?(Rails) && Rails.env == 'production' ? self.production_url : self.test_url
+    else
+      self.production_url
+    end
   end
 
   def self.notification(post, options = {})
